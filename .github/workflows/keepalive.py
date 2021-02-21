c9f1cd3d1e30c5ea81faeb45cbaf1a2ae2401228
rcsupermanjob <admin@rc.sb>
2021-02-21 12:47:24 +0800

    try

--- .github/workflows/keepalive.py
+++ .github/workflows/keepalive.py
@@ -113,7 +113,7 @@ def request(filename):
                     connect_17ce(f'wss://wsapi.17ce.com:8001/socket/?user=yiqice@qq.com&code={code}&ut={ut}', url_1))
             else:
                 print(url_1, 'cant request 17ce')
-            time.sleep(5)
+            time.sleep(10)
     except Exception as e:
         traceback.print_exc()
 
